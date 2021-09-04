3878cd73a522ae15cd15c3430918339c874195a5
thengo1 <tbn2109@columbia.edu>
2021-09-04 00:08:48 -0400

    start of twitter oauth

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const dotenv = require("dotenv");
 const app = express();
 
 dotenv.config();
+
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -14,6 +15,8 @@ mongoose.connect(process.env.MONGO_URL, {
 app.use(express.json());
 app.use(morgan("common"));
 
-app.listen(5000, () => {
-  console.log("Server is listening on port 5000...");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}...`);
 });
