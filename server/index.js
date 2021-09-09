5e8d4190dcd8088179eb303432e1d394c6df5cba
thengo1 <tbn2109@columbia.edu>
2021-09-09 00:42:29 -0400

    adding proxy

--- server/index.js
+++ server/index.js
@@ -4,10 +4,23 @@ const morgan = require("morgan");
 const dotenv = require("dotenv");
 const session = require("express-session");
 const grant = require("grant").express();
+const cors = require("cors");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+/*
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+*/
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("common"));
 
 app
   .use(session({ secret: "grant", saveUninitialized: true, resave: false }))
@@ -24,17 +37,6 @@ app
   .get("/hi", (req, res) => {
     res.end(JSON.stringify(req.session.grant.response, null, 2));
   });
-
-dotenv.config();
-/*
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-*/
-
-app.use(express.json());
-app.use(morgan("common"));
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}...`);
